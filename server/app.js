01fad93c3b4ea54461e062f056962ee1a21ad1a9
AJP <arvind.puvi@gmail.com>
2019-04-20 23:41:25 +1000

    Add styling

--- server/app.js
+++ server/app.js
@@ -3,8 +3,11 @@ const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 mongoose
   .connect(db, { useNewUrlParser: true })
